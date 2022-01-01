76aec7e70345b1c855334a7356821c0bdaf587d9
Shah-jahan Mukhtar <Shahjahanmukhtar62@gmail.com>
2022-01-01 15:09:39 +0500

    Navigation

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import React from "react";
-import {
-  View,
-  Text,
-  SafeAreaView,
-  StyleSheet,
-  SectionList,
-  FlatList,
-} from "react-native";
-import ColorBox from "./components/colorBox";
-import Foods from "./components/food";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/home";
+import ColorPalette from "./screens/ColorPalette";
+import { createStackNavigator } from "@react-navigation/stack";
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.screen name="Home" component={Home} />
+        <Stack.screen name="ColorPalette" component={ColorPalette} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
 
 // FlatLIST EXAMPLE
+
 // const COLORS = [
 //   { colorName: "Base03", hexCode: "#002b36" },
 //   { colorName: "Base02", hexCode: "#073642" },
@@ -32,68 +41,69 @@ import Foods from "./components/food";
 
 // SECTION LIST EXAMPLE:
 
-const FOODS = [
-  { title: "Healthy", data: ["Apples", "Broccoli"] },
-  { title: "Not so Healthy", data: ["Cookies", "Doritos", "Eclairs"] },
-];
+// const FOODS = [
+//   { title: "Healthy", data: ["Apples", "Broccoli"] },
+//   { title: "Not so Healthy", data: ["Cookies", "Doritos", "Eclairs"] },
+// ];
 
-const App = () => {
-  return (
-    <SafeAreaView
-      style={{ top: 60, alignItems: "center", justifyContent: "center" }}
-    >
-      {/* <View style={styles.container}>
-        <ColorBox colorName="yellow" hexCode="#fbb117" />
-        <ColorBox colorName="cyan" hexCode="#2aa198" />
-        <ColorBox colorName="blue" hexCode="#268bd2" />
-        <ColorBox colorName="Magenta" hexCode="#d33682" />
-      </View> */}
+//       {/* <SafeAreaView
+//         style={{ top: 60, alignItems: "center", justifyContent: "center" }}
+//       > */}
+//         {/* <View style={styles.container}>
+//         <ColorBox colorName="yellow" hexCode="#fbb117" />
+//         <ColorBox colorName="cyan" hexCode="#2aa198" />
+//         <ColorBox colorName="blue" hexCode="#268bd2" />
+//         <ColorBox colorName="Magenta" hexCode="#d33682" />
+//       </View> */}
 
-      {/* <FlatList
-        // style={styles.container}
-        data={COLORS}
-        keyExtractor={(item) => item.hexCode}
-        renderItem={({ item }) => (
-          <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
-        )}
-        // renderSectionHeader={({ section }) => (
-        //   <Text style={styles.header}>{section.title}:</Text>
-        // )}
-        ListHeaderComponent={<Text style={styles.heading}>Solarized</Text>}
-      /> */}
+//         {/* <FlatList
+//           // style={styles.container}
+//           data={COLORS}
+//           keyExtractor={(item) => item.hexCode}
+//           renderItem={({ item }) => (
+//             <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
+//           )}
+//           // renderSectionHeader={({ section }) => (
+//           //   <Text style={styles.header}>{section.title}:</Text>
+//           // )}
+//           ListHeaderComponent={<Text style={styles.heading}>Solarized</Text>}
+//         /> */}
 
-      <SectionList
-        sections={FOODS}
-        keyExtractor={(item) => item}
-        renderItem={(data) => <Foods name={data.item} />}
-        renderSectionHeader={({ section }) => (
-          <Text style={styles.heading}>{section.title}</Text>
-        )}
-      />
-    </SafeAreaView>
-  );
-};
+//         {/* <SectionList
+//         sections={FOODS}
+//         keyExtractor={(item) => item}
+//         renderItem={(data) => <Foods name={data.item} />}
+//         renderSectionHeader={({ section }) => (
+//           <Text style={styles.heading}>{section.title}</Text>
+//         )}
+//       /> */}
+//       {/* </SafeAreaView> */}
 
-const styles = StyleSheet.create({
-  container: {
-    // alignItems: "center",
-    // justifyContent: "center",
-    paddingTop: 30,
-    paddingHorizontal: 8,
-    paddingBottom: 10,
-  },
-  // text: {
-  //   fontWeight: "bold",
-  //   fontSize: 18,
-  //   color: "black",
-  // },
-  heading: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "pink",
-    paddingHorizontal: 5,
-    backgroundColor: "teal",
-  },
-});
+// {/* }; */}
 
-export default App;
+// // const styles = StyleSheet.create({
+// //   container: {
+// //     // alignItems: "center",
+// //     // justifyContent: "center",
+// //     paddingTop: 30,
+// //     paddingHorizontal: 8,
+// //     paddingBottom: 10,
+// //   },
+//   // text: {
+//   //   fontWeight: "bold",
+//   //   fontSize: 18,
+//   //   color: "black",
+//   // },
+// //   heading: {
+// //     fontSize: 30,
+// //     fontWeight: "bold",
+// //     color: "black",
+// //     paddingHorizontal: 5,
+
+// //     borderRadius: 5,
+// //     justifyContent: "center",
+// //     alignItems: "center",
+// //     marginBottom: 10,
+// //     marginTop: 10,
+// //   },
+// // });
--- components/food.js
+++ components/food.js
@@ -13,19 +13,21 @@ const styles = StyleSheet.create({
   container: {
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "teals",
+
     color: "black",
     padding: 5,
-
+    backgroundColor: "teal",
     marginBottom: 6,
-
     width: 320,
-    height: 300,
+    height: 200,
+    borderBottomRightRadius: 50,
+    borderTopLeftRadius: 50,
   },
   text: {
     alignItems: "center",
     justifyContent: "center",
-    fontSize: 18,
+    fontSize: 25,
+    fontWeight: "bold",
   },
 });
 
